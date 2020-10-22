06400e22d83c1207868aa80e3aeb4047fc40e2b0
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 13:51:39 +0530

    Added: The player checks for a option.They are No Play,Ladder or Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,17 +3,52 @@ package com.snakeladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-	static int player1 = 0;
+	public static final int NOPLAY = 1;
+	public static final int LADDER = 2;
+	public static final int SNAKE = 3;
 
-	public static int rollDice() {
+	public static int position = 0;
+
+	int player1 = 0;
+
+	public static int rollDice(int num) {
 		int n = 0;
 		Random r = new Random();
-		n = r.nextInt(7);
+		n = r.nextInt(num);
 		return (n == 0 ? 1 : n);
 	}
 
+	public static void startGame() {
+		int random = rollDice(4);
+		int rolldice = rollDice(7);
+		System.out.println("---------------------");
+		System.out.println("Dice Value : " + rolldice);
+		System.out.println("Random value : " + random);
+		switch (random) {
+		case NOPLAY:
+			System.out.println("NOPLAY");
+			System.out.println("Position of player : " + position);
+			System.out.println("----------------------");
+			break;
+		case LADDER:
+			System.out.println("############## Climb up the ladder ############");
+			position += rolldice;
+			System.out.println("Position of the  Player = " + position);
+			System.out.println("----------------------");
+			break;
+		case SNAKE:
+			System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
+			position -= rolldice;
+			System.out.println("Position of the  Player = " + position);
+			System.out.println("----------------------");
+			break;
+		default:
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
-		int rollDice = rollDice();
-		System.out.println("Dice Value: " + rollDice);
+		System.out.println("Player Position At Start: " + position);
+		startGame();
 	}
 }
