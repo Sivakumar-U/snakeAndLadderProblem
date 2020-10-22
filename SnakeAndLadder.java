92fbe64dd26f65f2c522b35e92feb3f864a91740
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 13:24:24 +0530

    Added: The player rolls the die to get the number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,19 @@
 package com.snakeladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 	static int player1 = 0;
+
+	public static int rollDice() {
+		int n = 0;
+		Random r = new Random();
+		n = r.nextInt(7);
+		return (n == 0 ? 1 : n);
+	}
+
 	public static void main(String[] args) {
+		int rollDice = rollDice();
+		System.out.println("Dice Value: " + rollDice);
 	}
 }
