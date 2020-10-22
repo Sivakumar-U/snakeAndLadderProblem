e5520141d400ba7aa4569cbbd5bf3063b87c1552
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 14:46:22 +0530

    Added: To repeat till the player reaches the winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,7 @@ public class SnakeAndLadder {
 	public static final int NOPLAY = 1;
 	public static final int LADDER = 2;
 	public static final int SNAKE = 3;
+	final static int WINPOINT = 100;
 
 	public static int position = 0;
 
@@ -19,36 +20,44 @@ public class SnakeAndLadder {
 	}
 
 	public static void startGame() {
-		int random = rollDice(4);
-		int rolldice = rollDice(7);
-		System.out.println("---------------------");
-		System.out.println("Dice Value : " + rolldice);
-		System.out.println("Random value : " + random);
-		switch (random) {
-		case NOPLAY:
-			System.out.println("NOPLAY");
-			System.out.println("Position of player : " + position);
-			System.out.println("----------------------");
-			break;
-		case LADDER:
-			System.out.println("############## Climb up the ladder ############");
-			position += rolldice;
-			System.out.println("Position of the  Player = " + position);
-			System.out.println("----------------------");
-			break;
-		case SNAKE:
-			System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
-			position -= rolldice;
-			System.out.println("Position of the  Player = " + position);
-			System.out.println("----------------------");
-			break;
-		default:
-			break;
-		}
+		do {
+			int random = rollDice(4);
+			int rolldice = rollDice(7);
+			System.out.println("---------------------");
+			System.out.println("Dice Value : " + rolldice);
+			System.out.println("Random value : " + random);
+			switch (random) {
+			case NOPLAY:
+				System.out.println("NOPLAY");
+				System.out.println("Position of player : " + position);
+				System.out.println("----------------------");
+				break;
+			case LADDER:
+				System.out.println("############## Climb up the ladder ############");
+				position += rolldice;
+				System.out.println("Position of the  Player = " + position);
+				System.out.println("----------------------");
+				break;
+			case SNAKE:
+				System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
+				position -= rolldice;
+				System.out.println("Position of the  Player = " + position);
+				System.out.println("----------------------");
+				break;
+			default:
+				break;
+			}
+			if (position < 0) {
+				position = 0;
+			}
+		} while (position != WINPOINT);
 	}
 
 	public static void main(String[] args) {
+		System.out.println("------------------Start--------------------");
+		
 		System.out.println("Player Position At Start: " + position);
 		startGame();
+		System.out.println("-------------------End----------------------");
 	}
 }
